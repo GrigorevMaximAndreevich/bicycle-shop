0a72cfaad26045ae4e554586073f94468abd9f84
GrigorevMaximAndreevich <maxgarik1@mail.ru>
2026-08-12 19:35:25 +0300

    Инициализация проекта

---
+++ src/main/java/com/github/grigorevmaximandreevich/bicycleshop/BicycleShopApplication.java
@@ -0,0 +1,13 @@
+package com.github.grigorevmaximandreevich.bicycleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BicycleShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/grigorevmaximandreevich/bicycleshop/BicycleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.grigorevmaximandreevich.bicycleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
